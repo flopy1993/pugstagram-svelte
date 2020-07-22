4f030ccc3799c2c6ac0f579283ec5abfc96a71e3
flopy1993 <flopy1993@gmail.com>
2020-07-22 13:52:49 -0300

    pugstagram

---
+++ src/components/Footer.svelte
@@ -0,0 +1,17 @@
+<style>
+  .Footer {
+    font-size: 11px;
+    letter-spacing: 1px;
+    font-weight: normal;
+    color: #c7c7c7;
+  }
+</style>
+
+<div class="Footter">
+  <div class="Footer-container">
+    <div class="Footer-copy">
+      <h3>2020 pugstram from platzi</h3>
+    </div>
+
+  </div>
+</div>
---
+++ src/containers/App.svelte
@@ -0,0 +1,35 @@
+<script>
+  import { onMount } from "svelte";
+  import Header from "../components/Header.svelte";
+  import Main from "../components/Main.svelte";
+  import TimeLine from "../components/TimeLine.svelte";
+  import SideBar from "../components/SideBar.svelte";
+
+  let data = {};
+  let API = "https://us-central1-pugstagram-co.cloudfunctions.net/data";
+  onMount(async () => {
+    const response = await fetch(API);
+    data = await response.json();
+  });
+</script>
+
+<style>
+  :global(body) {
+    background-color: #fafafa;
+    color: rgba(38, 38, 38, 0.7);
+    font-family: "Times New Roman", Times, serif;
+    margin: 0;
+    padding: 0;
+  }
+
+  :global(h1, h2, h3) {
+    margin: 0;
+    padding: 0;
+  }
+</style>
+
+<Header />
+<Main>
+  <TimeLine posts={data.posts} />
+  <SideBar {...data.user} />
+</Main>
